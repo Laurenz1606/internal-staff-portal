692d129ee79ab56a5689e8993dd2f342ebc35028
Laurenz1606 <laurenz.rausche@gmail.com>
2022-02-12 14:25:06 +0100

    added basic routing

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,29 @@
+import { Guard } from "@authfunctions/react";
 import React from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route
+          path="/login"
+          element={
+            <Guard type="LoggedOutOnly">
+              <>Hallo</>
+            </Guard>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Guard type="LoggedInOnly">
+              <Outlet />
+            </Guard>
+          }
+        >
+          <Route element={<>Dashboard</>} index />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
